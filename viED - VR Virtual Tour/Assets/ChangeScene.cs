c0d9772bbca35c3fba22e35b13837d350dc02cdc
mikulangots <34790922+mikulangots@users.noreply.github.com>
2020-08-23 00:54:51 +1000

    Working Build v1.0
    First draft of working build
    Can be build for Android, for VR experience Cardboard needed

---
+++ viED - VR Virtual Tour/Assets/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public GameObject ThisPanorama;
+    public GameObject TargetPanorama;
+
+    //public void GoToA()
+    //{
+    //    SetSkyBox();
+    //}
+
+    //public void GoToB()
+    //{
+    //    SetSkyBox();
+    //}
+
+    public void ChangeHotSpot()
+    {
+        if (TourManager.SetCameraPosition != null)
+            TourManager.SetCameraPosition(TargetPanorama.transform.position, ThisPanorama.transform.position);
+        TargetPanorama.gameObject.SetActive(true);
+        ThisPanorama.gameObject.SetActive(false);
+    }
+}
