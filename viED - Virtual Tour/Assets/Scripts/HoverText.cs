af9e68c58b1dcddef0729e20f36005e80183d440
masonAIT <masoncch@users.noreply.github.com>
2020-09-19 16:30:36 +1000

    Added moving scene function

---
+++ viED - Virtual Tour/Assets/Scripts/HoverText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverText : MonoBehaviour
+{
+    public GameObject textHover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textHover.SetActive(false);
+    }
+
+    void OnMouseOver()
+    {
+        textHover.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        textHover.SetActive(false);
+    }
+
+    
+}
